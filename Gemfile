bf85bbe1b57fd7d749b6de77437dbd793935776b
michelle <chelinbracho@gmail.com>
2019-06-23 12:31:51 -0400

    added bcrypt gem for user authentication

--- Gemfile
+++ Gemfile
@@ -32,6 +32,10 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', ">= 4.3.1"
 gem 'autoprefixer-rails'
 gem 'jquery-rails'
+
+# Use ActiveModel has_secure_password
+gem 'bcrypt', '~> 3.1.7'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :hashts
-  resources :categoria
-  resources :posts
-  resources :usuarios
-  root 'landing#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	#get '/losamo', to: 'login#index'
+  	
+  	resources :hashts
+  	resources :categoria
+  	resources :posts
+  	resources :usuarios
+  	
+  	root 'landing#index'
+  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
